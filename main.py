fa7f5e9a1e995c7caf49afb22d3a05f25bc6dcdb
Craig Hurley <Craig@vegainformatics.com>
2025-04-09 16:51:26 -0400

    make main continuous and make log output more readable

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import re
 
 
-def search_keywords(text, keywords):
-    pattern = r'\b(?:' + '|'.join(map(re.escape, keywords)) + r')\b'
+def search_report_text(text, keywords):
+    pattern = r'\b(?:' + '|'.join(map(re.escape, keywords)) + r')'
     found_keywords = re.findall(pattern, text, flags=re.IGNORECASE)
     if found_keywords:
         print(f"Found the following keywords: {', '.join(set(found_keywords))}")
@@ -11,8 +11,14 @@ def search_keywords(text, keywords):
 
 
 if __name__ == '__main__':
-    report_text = input("Please enter the report text: ")
-    search_keywords(report_text, ['left'])
-    search_keywords(report_text, ['right'])
-    search_keywords(report_text, ['benign'])
-    search_keywords(report_text, ['malignant'])
+    while True:
+        report_text = input("Please enter the report text: ")
+        if report_text == 'quit':
+            break
+
+        print('\n')
+        print(re.sub(r'(?<=\.)\s+', '\n', report_text))
+        search_report_text(report_text, ['left'])
+        search_report_text(report_text, ['right'])
+        search_report_text(report_text, ['benign'])
+        search_report_text(report_text, ['malignant'])
