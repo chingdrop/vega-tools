fe5bdf2fb3d8cbc944a66ce3227ca312238c63ea
Craig Hurley <Craig@vegainformatics.com>
2025-04-09 15:25:09 -0400

    change to pandas

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import re
+import pandas as pd
+from pathlib import Path
 
+
+def search_keywords(value, keywords):
+    if isinstance(keywords, str):
+        keywords = [keywords]
+    pattern = r'\b(?:' + '|'.join(map(re.escape, keywords)) + r')\b'
+    found_keywords = re.findall(pattern, value, flags=re.IGNORECASE)
+    return set(found_keywords)
+
+
+def search_single_word(text, keywords):
+    res = search_keywords(text, keywords)
+    if res:
+        res = str(res).title()
+    return res
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    data_path = Path.cwd() / 'data'
+    df = pd.read_excel(data_path / "PRJ116405_ClientFacing_Reference_SpreadsheetvB.xlsx")
+    df['BiopsySide'] = df['ReportText'].apply(lambda x: search_single_word(x, 'left'))
+    df['BiopsySide'] = df['ReportText'].apply(lambda x: search_single_word(x, 'right'))
+    df['BiopsyResult'] = df['ReportText'].apply(lambda x: search_single_word(x, 'benign'))
+    df['BiopsyResult'] = df['ReportText'].apply(lambda x: search_single_word(x, 'malignant'))
