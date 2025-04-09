b44729f3acca2bf7d2a3939f4a528a676eef5bbd
Craig Hurley <Craig@vegainformatics.com>
2025-04-09 17:01:33 -0400

    better console readability

--- main.py
+++ main.py
@@ -7,17 +7,20 @@ def search_report_text(text, keywords):
     if found_keywords:
         print(f"Found the following keywords: {', '.join(set(found_keywords))}")
     else:
-        print("No keywords found.")
+        print("No keywords found")
 
 
 if __name__ == '__main__':
     while True:
+        print(('-' * 79), '\n')
         report_text = input("Please enter the report text: ")
         if report_text == 'quit':
             break
 
         print('\n')
         print(re.sub(r'(?<=\.)\s+', '\n', report_text))
+        print('\n')
+
         search_report_text(report_text, ['left'])
         search_report_text(report_text, ['right'])
         search_report_text(report_text, ['benign'])
