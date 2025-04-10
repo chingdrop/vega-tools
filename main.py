f0dd12fecd78f39b1afc7887652bde8bca4d7908
Craig Hurley <Craig@vegainformatics.com>
2025-04-09 20:03:08 -0400

    add function for line regex searching and add extract keyword for wire localization

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import re
+from rich.console import Console
+from rich.text import Text
 
 
-def search_report_text(text, keywords):
+def extract_keywords(text, keywords):
     pattern = r'\b(?:' + '|'.join(map(re.escape, keywords)) + r')'
     found_keywords = re.findall(pattern, text, flags=re.IGNORECASE)
     if found_keywords:
         print(f"Found the following keywords: {', '.join(set(found_keywords))}")
-    else:
-        print("No keywords found")
+
+
+def print_line_with_keywords(text, keywords):
+    split_text = re.split(r'(?<=\.)\s+', text)
+    pattern = r'(?i)^.*\b(?:' + '|'.join(map(re.escape, keywords)) + r')\b'
+    for line in split_text:
+        if re.match(pattern, line, flags=re.IGNORECASE):
+            print(line.title())
 
 
 if __name__ == '__main__':
+    console = Console()
     while True:
         print(('-' * 79), '\n')
         report_text = input("Please enter the report text: ")
@@ -18,10 +27,15 @@ if __name__ == '__main__':
             break
 
         print('\n')
-        print(re.sub(r'(?<=\.)\s+', '\n', report_text))
+        report_text = re.sub(r'(?<=\.)\s+', '\n', report_text).title()
+        print(report_text)
         print('\n')
 
-        search_report_text(report_text, ['left'])
-        search_report_text(report_text, ['right'])
-        search_report_text(report_text, ['benign'])
-        search_report_text(report_text, ['malignant'])
+        print_line_with_keywords(report_text, ['Impression:', 'Pathology'])
+        print('\n')
+
+        extract_keywords(report_text, ['left'])
+        extract_keywords(report_text, ['right'])
+        extract_keywords(report_text, ['benign'])
+        extract_keywords(report_text, ['malignant'])
+        extract_keywords(report_text, ['wire', 'localization'])
