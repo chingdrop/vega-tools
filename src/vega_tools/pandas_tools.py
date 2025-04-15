657e27e224eefddd62c4a2a059fec6bd0411da1b
Craig Hurley <craig@vegainformatics.com>
2025-04-15 13:42:15 -0400

    Use OpenPxl as the Excel engine for Pandas. (#4)

--- src/vega_tools/pandas_tools.py
+++ src/vega_tools/pandas_tools.py
@@ -3,19 +3,22 @@ import pandas as pd
 from pathlib import Path
 
 
-def search_keywords(value, keywords):
-    if isinstance(keywords, str):
-        keywords = [keywords]
-    pattern = r'\b(?:' + '|'.join(map(re.escape, keywords)) + r')\b'
-    found_keywords = re.findall(pattern, value, flags=re.IGNORECASE)
-    return set(found_keywords)
+def read_excel_file(file_path: str | Path, sheet_name: str | int=0):
+    """
+    Reads an Excel file into a pandas DataFrame.
 
+    Parameters:
+    - file_path (str or Path): Path to the Excel file.
+    - sheet_name (str or int, default=0): Name or index of the sheet to read.
 
-def search_single_word(text, keywords):
-    res = search_keywords(text, keywords)
-    if res:
-        res = str(res).title()
-    return res
+    Returns:
+    - pd.DataFrame: The data from the Excel sheet as a DataFrame.
+    """
+    try:
+        return pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl')
+    except Exception as e:
+        print(f'Error reading Excel file: {e}')
+        return None
 
 if __name__ == '__main__':
     data_path = Path.cwd() / 'data'
