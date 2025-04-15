f340b9468106596e26bbb86d2183241bc8b6a946
Craig Hurley <craig@vegainformatics.com>
2025-04-15 15:06:06 -0400

    Test GPG signing on commits

--- src/vega_tools/utils/regex_patterns.py
+++ src/vega_tools/utils/regex_patterns.py
@@ -5,4 +5,4 @@ from typing import List
 # ToDo - Add docstrings to better describe Regex operations.
 def create_keywords_pattern(keywords: List[str]) -> Pattern:
     # noinspection RegExpUnnecessaryNonCapturingGroup
-    return re.compile(fr'\\b(?:{"|".join(map(re.escape, keywords))})', re.IGNORECASE)
+    return re.compile(fr'\b(?:{"|".join(map(re.escape, keywords))})', re.IGNORECASE)
