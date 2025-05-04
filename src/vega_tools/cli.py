b8610490f5375a4fedfc228d769890a5995ebc03
Craig Hurley <craig@vegainformatics.com>
2025-05-03 23:37:15 -0400

    Use variables to set common values and create necessary directories (#8)

--- src/vega_tools/cli.py
+++ src/vega_tools/cli.py
@@ -1,7 +1,15 @@
+from pathlib import Path
+
 from vega_tools.commands import cli
+from vega_tools.utils.files_and_storage import create_directory
+
+
+PROJECT_DIRECTORY = Path.cwd().parent
+DATA_DIRECTORY = PROJECT_DIRECTORY / 'data'
 
 
 def main():
+    create_directory(DATA_DIRECTORY)
     cli()
 
 if __name__ == '__main__':
