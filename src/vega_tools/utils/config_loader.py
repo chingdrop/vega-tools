1882d7aec7efab24b4784e7ef257d389581b2109
Craig Hurley <craig@vegainformatics.com>
2025-05-08 09:35:31 -0400

    Add docstrings to new class in utils folder (#11)

--- src/vega_tools/utils/config_loader.py
+++ src/vega_tools/utils/config_loader.py
@@ -1,16 +1,36 @@
 import json
 from pathlib import Path
+from typing import Dict, Any
 
 
-# ToDo - Add docstrings for class.
 class ConfigLoader:
+    """
+    Loads a JSON configuration file to a dictionary.
+
+    Args:
+        filepath (Path | str): File path to configuration file.
+
+    Attributes:
+        config (dict): The configuration dictionary.
+    """
+
     def __init__(self, filepath: Path | str):
         if isinstance(filepath, str):
             filepath = Path(filepath)
         self.filepath = filepath
         self.config = self._load()
 
-    def _load(self):
+    def _load(self) -> Dict[str, Any]:
+        """
+        Loads the configuration file into a dictionary from a JSON file.
+
+        Returns:
+            Dict[str, Any]: The configuration dictionary.
+
+        Raises:
+            FileNotFoundError: If the configuration file does not exist.
+            ValueError: If the configuration file is malformed.
+        """
         if not self.filepath.exists():
             raise FileNotFoundError(f"Config file not found: {self.filepath}")
         with open(self.filepath, 'r') as file:
