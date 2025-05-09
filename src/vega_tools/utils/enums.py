696c6af7be0bf74224db021f2cecaf7d41a43f59
Craig Hurley <craig@vegainformatics.com>
2025-05-09 16:35:17 -0400

    No use having a generator, changing census name function to return list (#10)

--- src/vega_tools/utils/enums.py
+++ src/vega_tools/utils/enums.py
@@ -1,25 +1,18 @@
-from typing import Iterator
+from typing import List
+
+import pandas as pd
 
 from vega_tools.api_tools import CensusNamesApi
-from vega_tools.settings import DATA_DIRECTORY
-from vega_tools.utils.files_and_storage import read_text_from_file
 
 
-def generate_common_names() -> Iterator[str]:
-    """
-    Generator of common names using word list from the 2010 census.
+def load_census_names(year: str = "2010") -> List[str]:
+    api = CensusNamesApi(year=year)
+    path = api.get_save_file()
+    if not path.exists():
+        api.download_name_list()
 
-    Returns:
-        Iterator[str]: The generator of common names.
-    """
-    census_api = CensusNamesApi(year='2010')
-    save_file = census_api.get_save_file()
-    if not save_file.exists():
-        census_api.download_name_list()
-
-    names = read_text_from_file(DATA_DIRECTORY / 'census_names.txt')
-    for name in names:
-        yield name.title()
+    df = pd.read_csv(path, header=None, names=["name"])
+    return [n.title() for n in df["name"]]
 
 
 DICOM_2D_SERIES_DESCRIPTIONS = {
