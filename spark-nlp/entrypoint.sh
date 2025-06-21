60aa519937185f7835588016416833b6a3ba4f32
Craig Hurley <craig@vegainformatics.com>
2025-06-21 14:16:37 -0500

    use entrypoint to finish setup

--- spark-nlp/entrypoint.sh
+++ spark-nlp/entrypoint.sh
@@ -1,20 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-export_json () {
-    for s in $(echo $values | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' $1 ); do
-        export $s
-    done
-}
+# If you want to pin spark-ocr or spark-nlp-jsl to a version different
+# from PUBLIC_VERSION, define OCR_VERSION or JSL_VERSION in your .env.
+# Otherwise they default to PUBLIC_VERSION.
+OCR_VER=${OCR_VERSION:-$PUBLIC_VERSION}
+JSL_VER=${JSL_VERSION:-$PUBLIC_VERSION}
 
-export_json "/notebooks/sparknlp_keys.json"
+echo "Installing spark-ocr==${OCR_VER} from private index"
+pip install --upgrade -q spark-ocr==${OCR_VER} \
+    --extra-index-url=https://pypi.johnsnowlabs.com/${SPARK_OCR_LICENSE}
 
-pip install --upgrade spark-nlp-jsl==$JSL_VERSION --user --extra-index-url https://pypi.johnsnowlabs.com/$SECRET
+echo "Installing pyspark==3.4.0 and spark-nlp==${PUBLIC_VERSION}"
+pip install --upgrade -q pyspark==3.4.0 spark-nlp==${PUBLIC_VERSION}
 
-if [ $? != 0 ];
-then
-    exit 1
-fi
+echo "Installing spark-nlp-jsl==${JSL_VER} from private index"
+pip install --upgrade -q spark-nlp-jsl==${JSL_VER} \
+    --extra-index-url=https://pypi.johnsnowlabs.com/${SPARK_NLP_LICENSE}"
 
-pip install --upgrade spark-nlp-display
-
-jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password='' --allow-root
+# Finally, launch Jupyter (or whatever your CMD is)
+exec jupyter notebook \
+  --port=8888 \
+  --no-browser \
+  --ip=0.0.0.0 \
+  --NotebookApp.token='' \
+  --NotebookApp.password='' \
+  --allow-root
