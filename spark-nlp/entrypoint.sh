e60dfa7e31d266c18ba96654ad9d87e2c4c3de04
Craig Hurley <craig@vegainformatics.com>
2025-06-24 19:49:23 -0500

    upgrade docker build process

--- spark-nlp/entrypoint.sh
+++ spark-nlp/entrypoint.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+set -e
+
 if [ ! -z "$JSL_VERSION" ]; then
-    echo "Installing spark-nlp-jsl packages..."
-    pip install --upgrade spark-nlp-jsl==$JSL_VERSION --user --extra-index-url https://pypi.johnsnowlabs.com/$SECRET
+    echo "Installing spark-nlp-jsl==$JSL_VERSION..."
+    pip3 install --no-cache-dir spark-nlp-jsl==$JSL_VERSION \
+        --extra-index-url https://pypi.johnsnowlabs.com/$SECRET
 fi
+
 if [ ! -z "$OCR_VERSION" ]; then
-    echo "Installing spark-ocr packages..."
-    pip install --upgrade spark-ocr==$OCR_VERSION --user --extra-index-url https://pypi.johnsnowlabs.com/$SPARK_OCR_SECRET
+    echo "Installing spark-ocr==$OCR_VERSION..."
+    pip3 install --no-cache-dir spark-ocr==$OCR_VERSION \
+        --extra-index-url https://pypi.johnsnowlabs.com/$SPARK_OCR_SECRET
 fi
 
-if [ $? != 0 ];
-then
-    exit 1
-fi
+echo "Installing spark-nlp-display..."
+pip3 install --no-cache-dir spark-nlp-display
 
-pip install --upgrade spark-nlp-display
-
-jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --NotebookApp.token='' --NotebookApp.password='' --allow-root
+# Exec the command passed via CMD (e.g., Jupyter Notebook)
+exec "$@"
