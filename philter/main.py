4cf26321fc9ffc4fd051ab368a058a333176b058
Craig Hurley <craig@vegainformatics.com>
2025-06-25 16:24:18 -0500

    combine python files into one module, use functions to separate code.

--- philter/main.py
+++ philter/main.py
@@ -1,24 +1,56 @@
 # Import key operating system functionality
 import os
+import pandas as pd
+from pathlib import Path
 
-# Set up base directories which contain the input reports (as individual .txt files) and output directory (this can contain
-# either .txt or .xml files, depending on which PHILTER command line script is run)
-baseDir = f"C:/Users/luke/Desktop/test"
-txtInputDir = "C:/Users/luke/Desktop/test/input/"
-outputDir = "C:/Users/luke/Desktop/test/output/"
-# This is the default path to a configurations file required for PHILTER to run. It should use the default but I ran into issues unless the location is hardcoded
-philterDeltaPath = ('C:/Users/luke/PycharmProjects/pythonProject/philter-ucsf/configs/philter_delta.js'
-                    ''
-                    ''
-                    '\
-                    on')
-# This is simply the home folder of the PHILTER repository which we copied via github
-philterBaseDir = 'C:/Users/luke/PycharmProjects/pythonProject/philter-ucsf/'
 
-# Change to home directory of the cloned PHILTER repository to run the command from.
-os.chdir(philterBaseDir)
-# os.system(f'python main.py -i {txtInputDir} -o {outputDir} -f {philterDeltaPath} --prod=True --v=False') #XML FORMAT
-os.system(
-    f'python main.py -i {txtInputDir} -o {outputDir} -f {philterDeltaPath} --prod=True --outputformat "asterisk"')  # Asterisked Format
+base_path = Path(__file__).resolve().parent
 
-print("Done...")
+data_path = base_path / 'data'
+input_path = data_path / 'input'
+output_path = data_path / 'output'
+
+philter_base_path = base_path / 'philter-ucsf'
+philter_delta_path = philter_base_path / 'configs' / 'philter_delta.json'
+
+
+def csv_splitter(original_report):
+    df = pd.read_csv(original_report)
+    for index, row in df.iterrows():
+        accession = str(row['Accession'])
+        report = str(row['Reports'])
+        os.chdir(input_path)
+        print(f"Converting file: {accession}")
+        with open(f'{accession}.txt', 'w') as f:
+            f.write(report)
+
+
+def repackage_into_csv(result_report):
+    data = []
+    for filename in os.listdir(output_path):
+        if filename.endswith(".txt"):
+            with open(os.path.join(output_path, filename), 'r') as f:
+                contents = f.read()
+
+            data.append((filename, contents))
+
+    df = pd.DataFrame(data, columns=['Filename', 'Contents'])
+    df.to_csv(result_report, index=False)
+
+
+def main():
+    original_report_path = data_path / 'original_reports.csv'
+    result_report = data_path / 'result_report.csv'
+
+    csv_splitter(original_report_path)
+
+    os.chdir(philter_base_path)
+    os.system(f'python main.py -i {input_path} -o {output_path} -f {philter_delta_path} --prod=True --outputformat "asterisk"')
+
+    repackage_into_csv(result_report)
+
+    print("Done...")
+
+
+if __name__ == "__main__":
+    main()
